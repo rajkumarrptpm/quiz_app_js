ab821c15dc5fb17be09894c26e3a14546d83b7a0
Raj kumar R <78129713+rajkumarrptpm@users.noreply.github.com>
2025-01-26 16:04:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let quizData = [
+    {
+        question : "Most popular programing language?",
+        a : "C++",
+        b : "Python",
+        c : "Javascript",
+        d : "java",
+        answer : b
+    }, 
+]
+
+let question = document.getElementById("question")
+
+question.innerHTML = quizData[0].question
+
+
+function displayQuestionAnswers(){
+
+}
+
+displayQuestionAnswers()
